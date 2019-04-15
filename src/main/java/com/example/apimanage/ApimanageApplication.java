be993f4de5d1f94d91c024e0fa453196fe2bf647
BabyTitanium <in19951209@163.com>
2019-04-15 20:05:18 +0800

    init this project

---
+++ src/main/java/com/example/apimanage/ApimanageApplication.java
@@ -0,0 +1,13 @@
+package com.example.apimanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApimanageApplication.class, args);
+    }
+
+}
