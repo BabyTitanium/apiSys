0060a38a5ac2771ee4bc0154a664edd1c6522e4d
BabyTitanium <in19951209@163.com>
2019-04-16 20:42:33 +0800

    登录模块

--- src/main/java/com/example/apimanage/controller/UserController.java
+++ src/main/java/com/example/apimanage/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.apimanage.controller;
 
+import com.example.apimanage.ResultDetail.Result;
 import com.example.apimanage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +14,21 @@ public class UserController {
     @RequestMapping("getOneUser")
     @ResponseBody
     public String getOneUser(){
-        //return "111";
-
         Map<String,String> user=userService.getUserByName("张志明");
         return user.get("password");
     }
-
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Map<String, Object> login(@RequestBody Map<String, String> params){
-        System.out.println(params);
+        try{
+            Map<String, Object> result = userService.login(params);
+            if(result!=null){
+                return Result.successResult("登录成功");
+            }else{
+                return Result.errorResult("验证失败");
+            }
+        }catch (Exception e){
+            return Result.errorResult("服务器错误");
+        }
 
-        System.out.println(params.get("email"));
-
-        Map<String, Object> result = userService.login(params);
-
-        return result;
     }
 }
